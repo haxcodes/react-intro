e5a9e63ad6e6d62397b5980612d3d8961e5e4a0a
Aonghas Anderson <aonghas@gmail.com>
2017-02-26 13:36:45 +0000

    Hello World and webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist/assets',
+    filename: 'bundle.js',
+    publicPath: 'assets'
+  },
+  devServer: {
+    inline: true,
+    contentBase: './dist',
+    port: 3000
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loaders: 'babel-loader',
+        query: {
+          presets: ['latest', 'stage-0', 'react'] 
+        }
+      }]
+  }
+}
