ae95433f2296b4252c2accd8a27058cc161b74b3
Aonghas Anderson <aonghas@gmail.com>
2017-02-26 20:04:36 +0000

    Validating with proptypes

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -2,6 +2,7 @@
 import Terrain from 'react-icons/lib/md/terrain'
 import SnowFlake from 'react-icons/lib/ti/weather-snow'
 import Calendar from 'react-icons/lib/fa/calendar'
+import { PropTypes } from 'react'
 
 const percentToDecimal = (decimal) => {
   return ((decimal * 100) + '%')
@@ -11,7 +12,7 @@ const calcGoalProgress = (total, goal) => {
   return percentToDecimal(total/goal)
 }
 
-export const SkiDayCount = ({total, powder, backcountry, goal}) => (
+export const SkiDayCount = ({total=70, powder=20, backcountry=10, goal=100}) => (
   <div className="ski-day-count">
     <div className="total-days">
       <span>{total}</span>
@@ -37,3 +38,13 @@ export const SkiDayCount = ({total, powder, backcountry, goal}) => (
   </div>
 
 )
+
+SkiDayCount.defaultProps = {
+}
+
+SkiDayCount.propTypes = {
+  total: PropTypes.number.isRequired,
+  powder: PropTypes.number,
+  backcountry: PropTypes.number,
+  goal: PropTypes.number
+}
